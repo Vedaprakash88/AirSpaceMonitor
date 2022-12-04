56b954d5a7990887dd17f84e5130cd49925d564c
Veda Chintha <vedachintha@gmail.com>
2022-12-04 18:22:56 +0100

    Updates2 to Plots

--- plot.py
+++ plot.py
@@ -3,53 +3,47 @@ import pandas as pd
 from pandas import DataFrame
 import matplotlib.pyplot as plt
 
-df: DataFrame = pd.read_excel("NAT_Carrier.xlsx", sheet_name="Sheet1",
-                              index_col=0)
 
-df['Date'] = df['Date'].dt.strftime('%d/%m/%Y')
+class PlotFindings:
+    # Plot Date vs Altitude
+    @staticmethod
+    def plot_dt_v_alt(df: DataFrame) -> plt:
+        x_values = df.Date
+        y_values = df.Altitude
+        z_values = df.AC_Type
 
-# Plot Date vs Altitude
-def plot_dt_v_alt():
-    x_values = df.Date
-    y_values = df.Altitude
-    z_values = df.AC_Type
+        plt.scatter(x_values, y_values, color='blue', marker='o')
+        plt.title('NATO Aircraft @ Altitude')
+        plt.xlabel('Dates')
+        plt.ylabel('Altitude')
+        plt.grid()
+        plt.ylim([1000, 60000])
 
-    plt.scatter(x_values, y_values, color='blue', marker='o')
-    plt.title('NATO Aircraft @ Altitude')
-    plt.xlabel('Dates')
-    plt.ylabel('Altitude')
-    plt.grid()
-    plt.ylim([1000, 60000])
+        for x, y, z in zip(x_values, y_values, z_values):
+            label = str(z)
+            plt.annotate(label,  # this is the text
+                         (x, y),  # these are the coordinates to position the label
+                         textcoords="offset points",  # how to position the text
+                         xytext=(0, 10),  # distance from text to points (x,y)
+                         ha='left')  # horizontal alignment can be left, right or center
 
-    for x, y, z in zip(x_values, y_values, z_values):
-        label = str(z)
-        plt.annotate(label,  # this is the text
-                     (x, y),  # these are the coordinates to position the label
-                     textcoords="offset points",  # how to position the text
-                     xytext=(0, 10),  # distance from text to points (x,y)
-                     ha='left')  # horizontal alignment can be left, right or center
+        # plt.show()
+        return plt
 
-    plt.show()
-
-# Plot Date vs No.of Flights
-
-
-
-
+    # Plot Date vs No.of Flights
+    @staticmethod
+    def plot_dt_v_flight_count(df: DataFrame) -> plt:
+        x_values = df.Date
 
 
 
 
 
 
+        y_values = df.Altitude
+        z_values = df.AC_Type
+
+        return plt
 
 
 
-
-
-
-
-
-
-
-plot_dt_v_alt()
